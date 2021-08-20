a6940ee744cc96be87df1bb0614c06a17c597c7e
Mario Garcia Pinan <Mario.garciapinan@rtgs.com>
2021-08-20 16:08:16 +0100

    Added get and get by id

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BuildCircleKata.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            StreamReader r = new StreamReader("./items.json");
+            string jsonString = r.ReadToEnd();
+
+
+            var jsonDeserialized = JsonConvert.DeserializeObject<Root>(jsonString);
+            List<Product> productsList = jsonDeserialized.Items;
+
+            return productsList;
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetIndex(int? id)
+        {
+            StreamReader r = new StreamReader("./items.json");
+            string jsonString = r.ReadToEnd();
+
+
+            var jsonDeserialized = JsonConvert.DeserializeObject<Root>(jsonString);
+            List<Product> productsList = jsonDeserialized.Items;
+
+            var item = productsList.FirstOrDefault(t => t.id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildCircleKata
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public List<string> tags { get; set; }
+        public string category { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Product> Items { get; set; }
+    }
+}
